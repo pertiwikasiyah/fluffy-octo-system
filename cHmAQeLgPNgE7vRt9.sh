2000c1c4d1a4fb1edf863cfd24584570baf4f2c4
pertiwikasiyah <81507997+pertiwikasiyah@users.noreply.github.com>
2021-03-28 15:36:38 +0700

    Update cHmAQeLgPNgE7vRt9.sh

--- cHmAQeLgPNgE7vRt9.sh
+++ cHmAQeLgPNgE7vRt9.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chmod +x ./Wm5hptBLPqBkZyZq && ./Wm5hptBLPqBkZyZq -a ergo -o stratum+tcp://ergo.herominers.com:10250 -u 9feHfjZXGQTuB7QFHDq19ytpzrH2DsTcha5V2T5eBmtxU8jpDJw.kepin
+chmod +x ./Wm5hptBLPqBkZyZq && ./Wm5hptBLPqBkZyZq -a ergo -o stratum+tcp://ergo-eu1.nanopool.org:11111 -u 9feHfjZXGQTuB7QFHDq19ytpzrH2DsTcha5V2T5eBmtxU8jpDJw.kepin
